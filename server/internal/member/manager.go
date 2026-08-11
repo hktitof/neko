506e670f3ba5078009d8773b9fcf7ed1c5c307e6
titof <hktitof@gmail.com>
2026-08-11 16:53:43 +0100

    fix(session): reuse session object and token on login to prevent race condition

--- server/internal/member/manager.go
+++ server/internal/member/manager.go
@@ -147,10 +147,6 @@ func (manager *MemberManagerCtx) Login(username string, password string) (types.
 
 	session, ok := manager.sessions.Get(id)
 	if ok {
-		if manager.sessions.Settings().MercifulReconnect {
-			_ = manager.sessions.Delete(id)
-			return manager.sessions.Create(id, profile)
-		}
 		return session, session.Token(), nil
 	}
 
